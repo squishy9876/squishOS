c16406dd5703bca5c248ba286a37d450c9261646
new-hue <matthewmacaulay07@icloud.com>
2023-02-22 16:39:02 -0800

    mod browse

--- squishOS.interface/ModBrowse.cs
+++ squishOS.interface/ModBrowse.cs
@@ -57,9 +57,19 @@ public override async void OnShow(object[] args)
         {
             str.Repeat("=", SCREEN_WIDTH).AppendLine();
             str.BeginCenter().Append("squishOS").AppendLine();
-            str.Append("by squishy#9555")
+            str.Repeat("=", SCREEN_WIDTH);
+            str.Append("by squishy#9555");
             str.Repeat("=", SCREEN_WIDTH);
             var str = new StringBuilder();
             // 
-            str.AppendClr("[^ | v] SELECT MOD        [ENTER] INJECT/UNINJECT").AppendLine();
+            str.AppendClr("[^ | v] SELECT MOD   |   [ENTER] INJECT/UNINJECT").AppendLine();
         });
+
+        private void DrawNoMods(StringBuilder str)
+        {
+            // TODO: FINISH CHECKING DIRECTORY
+            str.Append("NO MODS FOUND.").Repeat("\n", 2);
+            str.Append("If you have mods in the folder").AppendLine();
+            str.Append(" make sure they are in the right format, .dll").Repeat("\n", 2);
+            str.BeginCenter().Append("PRESS ANY BUTTON TO CONTINUE...").EndAlign();
+        }
